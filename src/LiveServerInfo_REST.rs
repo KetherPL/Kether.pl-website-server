416c42cb45fa8c83ff75427b58b92d5ecc9f297b
Dominik Adrian Grzywak <starterx4@gmail.com>
2025-03-23 12:50:24 +0100

    Rusty beginnings

---
+++ src/LiveServerInfo_REST.rs
@@ -0,0 +1,115 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+#![allow(non_snake_case)]
+use gamedig::{games::l4d2, protocols::valve::game::Player};
+use rocket::{get, launch, routes, serde::{Deserialize, Serialize, json::Json}};
+
+// Define a struct to represent the L4D2 server response
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(crate = "rocket::serde")]
+struct L4D2ServerInfo {
+    name: String,
+    map: String,
+    players: u8,
+    maxplayers: u8,
+    bots: u8,
+    playerdetails: Vec<PD>,
+    // Add other fields as needed based on the gamedig response
+}
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(crate = "rocket::serde")]
+struct PD { // from gamedig::protocols::valve::game::Player
+    pub name: String,
+    pub score: i32,
+    pub duration: f32,
+}
+
+impl From<Player> for PD {
+    fn from(player: Player) -> Self {
+        PD {
+            name: player.name,
+            score: player.score,
+            duration: player.duration,
+        }
+    }
+}
+
+#[get("/<ip>/<port>")]
+fn live_server_info(ip: String, port: u16) -> Result<Json<L4D2ServerInfo>, String> {
+    // Get the IP address and port from the URL parameters
+
+    let response = l4d2::query(&ip.parse().unwrap(), Some(port));
+
+    match response { // Result type, must check what it is...
+        Err(error) => Err(format!("Couldn't query, error: {}", error)),
+        Ok(r) => {
+            // Extract the relevant information from the gamedig response
+            let server_info = L4D2ServerInfo {
+                name: r.name,
+                map: r.map,
+                players: r.players_online,
+                maxplayers: r.players_maximum,
+                bots: r.players_bots,
+                playerdetails: r.players_details.into_iter().map(PD::from).collect(),
+            };
+            Ok(Json(server_info))
+            // format!("{:?}", r)
+        },
+    }
+}
+
+
+#[get("/kether")]
+fn live_server_info_kether() -> Result<Json<L4D2ServerInfo>, String> {
+    /* Hardcoded for simplicity, in a real-world application, 
+      this should be fetched from a configuration file or a database.
+      LiveServer-hosted Kether.pl L4D2 server as of 2025.
+    */
+    let response = l4d2::query(&"51.83.217.86".parse().unwrap(), Some(29800));
+
+    match response { // Result type, must check what it is...
+        Err(error) => Err(format!("Couldn't query, error: {}", error)),
+        Ok(r) => {
+            // Extract the relevant information from the gamedig response
+            let server_info = L4D2ServerInfo {
+                name: r.name,
+                map: r.map,
+                players: r.players_online,
+                maxplayers: r.players_maximum,
+                bots: r.players_bots,
+                playerdetails: r.players_details.into_iter().map(PD::from).collect(),
+            };
+            Ok(Json(server_info))
+        },
+    }
+}
+
+// Direct JSON from LiveServer API
+// #[get("/lvs")]
+// fn live_server_info_kether() -> String {
+//     /* Hardcoded for simplicity, in a real-world application, 
+//       this should be fetched from a configuration file or a database.
+//       LiveServer-hosted Kether.pl L4D2 server as of 2025.
+//     */
+//     let response = l4d2::query(&"51.83.217.86".parse().unwrap(), Some(29800));
+
+//     match response { // Result type, must check what it is...
+//         Err(error) => format!("Couldn't query, error: {}", error),
+//         Ok(r) => {
+//             // Extract the relevant information from the gamedig response
+//             let server_info = L4D2ServerInfo {
+//                 name: r.name,
+//                 map: r.map,
+//                 players: r.players.len() as u8,
+//                 max_players: r.max_players,
+//                 bots: r.bots,
+//             };
+//             Ok(Json(server_info))
+//         },
+//     }
+// }
+
+#[launch]
+pub fn rocket() -> _ {
+    rocket::build().mount("/api/LiveServerInfo", routes![live_server_info, live_server_info_kether])
+}
---
+++ src/main.rs
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+#![allow(non_snake_case)]
+use clap::{Parser, Subcommand};
+use gamedig::games::l4d2;
+use std::thread;
+
+mod LiveServerInfo_REST;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Start the RESTful server service
+    #[arg(short, long)]
+    service: bool,
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// -i <IP address> -p <PORT>   Prints selected server query to the CLI/terminal
+    Query {
+        #[arg(short, long, value_name = "L4D2 SERVER IP ADDRESS")]
+        ip: String,
+        #[arg(short, long, value_name = "PORT")]
+        port: u16,
+    }
+}
+
+fn main() {
+    let args = Args::parse();
+
+    if args.service {
+        println!("Starting internal services server service");
+        // :: Start the internal services server ::
+        // Start the LiveServerInfo RESTful service
+        thread::spawn(move || {
+            LiveServerInfo_REST::main();
+            std::process::exit(0);
+        });
+        // Keep the main thread alive so the server thread can run
+        // Without this, the main thread exits immediately, killing the server thread.
+        loop {
+            thread::sleep(std::time::Duration::from_secs(1));
+        }
+
+        //... TODO
+    }
+
+    match &args.command {
+        Some(Commands::Query { ip, port }) => {
+            query_l4d2_server(&ip, *port);
+        }
+        None => {
+            if !args.service {
+                eprintln!("No command nor argument specified!");
+            }
+        }
+    }
+}
+
+fn query_l4d2_server(ip: &str, port: u16) {
+
+    // Get the IP address and port from command line arguments
+
+    let response = l4d2::query(&ip.parse().unwrap(), Some(port));
+    // None is the default port (which is 27015), could also be Some(27015)
+
+    match response { // Result type, must check what it is...
+        Err(error) => println!("Couldn't query, error: {}", error),
+        Ok(r) => println!("{:#?}", r)
+    }
+}

10bd34c5012946dfd7a7ac07c3a6de549d7c2998
Dominik Adrian Grzywak <starterx4@gmail.com>
2025-07-19 21:50:48 +0200

    Refactor the 'rocket::build()' function to support feature flags

--- src/REST.rs
+++ src/REST.rs
@@ -1,14 +1,18 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
+#[cfg(any(feature = "rest_steam", feature = "rest_call_for_sub"))]
 use crate::config::Config;
+#[cfg(feature = "rest_sqlite")]
 use crate::databases_rest::mount_database_routes;
+#[cfg(feature = "server_query")]
 use crate::LiveServerInfo::{live_server_info, live_server_info_kether};
+#[cfg(feature = "rest_steam")]
 use crate::steam_rest::mount_steam_routes;
-use rocket::{launch, routes};
+use rocket::{launch, routes, Build, Rocket};
 use rocket_cors::{AllowedOrigins, CorsOptions};
 
 #[launch]
-pub fn rocket() -> _ {
+pub fn rocket() -> Rocket<Build> {
 	// Configure CORS
 	let allowed_origins = AllowedOrigins::some_exact(&[
 		"http://localhost:3000", // Local Kether website 'npm run start'
@@ -25,17 +29,37 @@ pub fn rocket() -> _ {
 	.to_cors()
 	.unwrap();
 
-	let config = Config::load().expect("Failed to load configuration");
-	let db_pool = crate::db::database::establish_connection_pool();
-
-	rocket::build()
-		.manage(config)
-		.manage(db_pool)
+	let mut rocket_build = rocket::build()
 		.configure(rocket::Config::figment().merge(("port", 3001))) // NodeJS / React port
-		.mount("/api/LiveServerInfo", routes![live_server_info, live_server_info_kether])
-		.mount("/api", mount_database_routes())
-		.mount("/api/steam", mount_steam_routes()) // Mount the new steam routes
-		.mount("/api/callForSub", crate::call_for_sub_rest::mount_callForSub_routes())
-		.mount("/", crate::sat_specific_rest::mount_sat_specific_routes())
-		.attach(cors)
+		.attach(cors.clone());
+
+	#[cfg(any(feature = "rest_steam", feature = "rest_call_for_sub", feature = "rest_sqlite"))]
+	{
+		let config = Config::load().expect("Failed to load configuration");
+		rocket_build = rocket_build.manage(config);
+	}
+	#[cfg(feature = "rest_sqlite")]
+	{
+		let db_pool = crate::db::database::establish_connection_pool();
+		rocket_build = rocket_build.manage(db_pool);
+		rocket_build = rocket_build.mount("/api", mount_database_routes());
+	}
+	#[cfg(feature = "server_query")]
+	{
+		rocket_build = rocket_build.mount("/api/LiveServerInfo", routes![live_server_info, live_server_info_kether]);
+	}
+	#[cfg(feature = "rest_steam")]
+	{
+		rocket_build = rocket_build.mount("/api/steam", mount_steam_routes());
+	}
+	#[cfg(feature = "rest_call_for_sub")]
+	{
+		rocket_build = rocket_build.mount("/api/callForSub", crate::call_for_sub_rest::mount_callForSub_routes());
+	}
+	#[cfg(feature = "sat")]
+	{
+		rocket_build = rocket_build.mount("/", crate::sat_specific_rest::mount_sat_specific_routes());
+	}
+
+	rocket_build
 }
